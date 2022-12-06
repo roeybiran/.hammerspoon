d02a9650065e687dc96f53ae82f59c317e538b43
roeybiran <roeybiran@icloud.com>
2022-12-06 16:13:27 +0200

    feat: add URLHandler

--- Spoons/StatusBar.spoon/init.lua
+++ Spoons/StatusBar.spoon/init.lua
@@ -19,21 +19,7 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 obj.menuBarItem = nil
 local iconPath = script_path() .. "/statusicon.pdf"
 
-local KEY = "RBDefaultBrowserBundleID"
-
-local browsersBundleIDs = {"com.apple.Safari", "com.brave.Browser"}
-
-local function installURLHandler()
-	hs.urlevent.setDefaultHandler("http")
-	hs.urlevent.httpCallback = function(scheme, host, params, fullURL, senderPID)
-		local defaultBrowserID = hs.settings.get(KEY) or "com.apple.Safari"
-		hs.urlevent.openURLWithBundle(fullURL, defaultBrowserID)
-	end
-end
-
 function obj:start()
-	installURLHandler()
-
 	obj.menuBarItem =
 		hs.menubar.new():setIcon(iconPath):setMenu(
 		function()
@@ -68,23 +54,7 @@ function obj:start()
 				{title = "-"},
 				{
 					title = "Default Browser",
-					menu = (function()
-						local browsersMenuItems = {}
-						for _, value in ipairs(browsersBundleIDs) do
-							table.insert(
-								browsersMenuItems,
-								{
-									title = hs.application.nameForBundleID(value),
-									fn = function(_, _)
-										hs.settings.set(KEY, value)
-									end,
-									image = hs.image.imageFromAppBundle(value),
-									checked = hs.settings.get(KEY) == value
-								}
-							)
-						end
-						return browsersMenuItems
-					end)()
+					menu = spoon.URLHandler:generateMenuItem()
 				},
 				{title = "-"},
 				{
@@ -97,19 +67,6 @@ function obj:start()
 		end
 	)
 
-	hs.urlevent.bind(
-		"set-default-browser",
-		function(_, params, _)
-			if params and params.id then
-				local newDefaultBrowserID = params.id
-				local theApp = hs.application.get(newDefaultBrowserID)
-				hs.notify.show("Default Browser Changed", (theApp and theApp:name()) or params.id, "")
-				hs.settings.set(KEY, newDefaultBrowserID)
-			else
-				print("Unrecognized browser: " .. (params.id or "NULL"))
-			end
-		end
-	)
 	return self
 end
 
